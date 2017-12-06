e46de58e65f7d648be5ff9397ddc85afcc404717
a.korotkov <a.korotkov@ceteralabs.com>
2017-12-06 23:28:31 +0300

    Слайдер новинки

--- www/index.php
+++ www/index.php
@@ -104,38 +104,72 @@ $APPLICATION->SetTitle("Интернет-магазин \"Одежда\"");
             </div>
         </div>
     </div>
-    <div class="map text-center margin-bottom-10">
-        <div class="map__slide"><a class="dress dress_static" href="#">
-                <div class="margin-bottom-3"><img src="<?=StockMan\Config::STOCKMAN_TEMPLATE_PATH?>/i/d1.png" alt=""></div>
-                <div class="dress__title">ASHLEY WILLIAMS</div>
-                <div class="text-secondary">Полосатое платье-рубашка</div>
-                <div class="text-size-large">49 765. -      </div></a></div>
-        <div class="map__slide"><a class="dress dress_static" href="#">
-                <div class="margin-bottom-3"><img src="<?=StockMan\Config::STOCKMAN_TEMPLATE_PATH?>/i/d2.png" alt=""></div>
-                <div class="dress__title">Proenza schouler</div>
-                <div class="text-secondary">Платье в полоску</div>
-                <div class="text-size-large">26 136. -   </div></a></div>
-        <div class="map__slide"><a class="dress dress_static" href="#">
-                <div class="margin-bottom-3"><img src="<?=StockMan\Config::STOCKMAN_TEMPLATE_PATH?>/i/d3.png" alt=""></div>
-                <div class="dress__title">Proenza schouler</div>
-                <div class="text-secondary">Платье в полоску</div>
-                <div class="text-size-large">49 765. -      </div></a></div>
-        <div class="map__slide"><a class="dress dress_static" href="#">
-                <div class="margin-bottom-3"><img src="<?=StockMan\Config::STOCKMAN_TEMPLATE_PATH?>/i/d1.png" alt=""></div>
-                <div class="dress__title">ASHLEY WILLIAMS</div>
-                <div class="text-secondary">Полосатое платье-рубашка</div>
-                <div class="text-size-large">26 136. - </div></a></div>
-        <div class="map__slide"><a class="dress dress_static" href="#">
-                <div class="margin-bottom-3"><img src="<?=StockMan\Config::STOCKMAN_TEMPLATE_PATH?>/i/d2.png" alt=""></div>
-                <div class="dress__title">Proenza schouler</div>
-                <div class="text-secondary">Платье в полоску</div>
-                <div class="text-size-large">49 765. -      </div></a></div>
-        <div class="map__slide"><a class="dress dress_static" href="#">
-                <div class="margin-bottom-3"><img src="<?=StockMan\Config::STOCKMAN_TEMPLATE_PATH?>/i/d3.png" alt=""></div>
-                <div class="dress__title">Proenza schouler</div>
-                <div class="text-secondary">Платье в полоску</div>
-                <div class="text-size-large">26 136. -</div></a></div>
-    </div>
+    <?/*Новинки*/?>
+    <?
+    global $arrNewProducts;
+    $arrNewProducts = array("PROPERTY_".StockMan\Config::NEW_PRODUCT."_VALUE" => "да");
+    ?>
+    <?$APPLICATION->IncludeComponent(
+        "bitrix:news.list",
+        "newproducts",
+        Array(
+            "ACTIVE_DATE_FORMAT" => "d.m.Y",
+            "ADD_SECTIONS_CHAIN" => "Y",
+            "AJAX_MODE" => "N",
+            "AJAX_OPTION_ADDITIONAL" => "",
+            "AJAX_OPTION_HISTORY" => "N",
+            "AJAX_OPTION_JUMP" => "N",
+            "AJAX_OPTION_STYLE" => "Y",
+            "CACHE_FILTER" => "N",
+            "CACHE_GROUPS" => "Y",
+            "CACHE_TIME" => "36000000",
+            "CACHE_TYPE" => "A",
+            "CHECK_DATES" => "Y",
+            "DETAIL_URL" => "",
+            "DISPLAY_BOTTOM_PAGER" => "N",
+            "DISPLAY_DATE" => "Y",
+            "DISPLAY_NAME" => "Y",
+            "DISPLAY_PICTURE" => "Y",
+            "DISPLAY_PREVIEW_TEXT" => "Y",
+            "DISPLAY_TOP_PAGER" => "N",
+            "FIELD_CODE" => array(
+                0 => "DETAIL_PICTURE",
+                1 => ""
+            ),
+            "FILTER_NAME" => "",
+            "HIDE_LINK_WHEN_NO_DETAIL" => "N",
+            "IBLOCK_TYPE" => StockMan\Config::CATALOG_TYPE,
+            "IBLOCK_ID" => StockMan\Config::CATALOG_ID,
+            "INCLUDE_IBLOCK_INTO_CHAIN" => "Y",
+            "INCLUDE_SUBSECTIONS" => "Y",
+            "MESSAGE_404" => "",
+            "NEWS_COUNT" => "",
+            "PAGER_BASE_LINK_ENABLE" => "N",
+            "PAGER_DESC_NUMBERING" => "N",
+            "PAGER_DESC_NUMBERING_CACHE_TIME" => "36000",
+            "PAGER_SHOW_ALL" => "N",
+            "PAGER_SHOW_ALWAYS" => "N",
+            "PAGER_TEMPLATE" => ".default",
+            "PAGER_TITLE" => "Новости",
+            "PARENT_SECTION" => "",
+            "PARENT_SECTION_CODE" => "",
+            "PREVIEW_TRUNCATE_LEN" => "",
+            "PROPERTY_CODE" => array("NEWPRODUCT", StockMan\Config::BREND, ""),
+            "SET_BROWSER_TITLE" => "Y",
+            "SET_LAST_MODIFIED" => "N",
+            "SET_META_DESCRIPTION" => "Y",
+            "SET_META_KEYWORDS" => "Y",
+            "SET_STATUS_404" => "N",
+            "SET_TITLE" => "Y",
+            "SHOW_404" => "N",
+            "SORT_BY1" => "ACTIVE_FROM",
+            "SORT_BY2" => "SORT",
+            "SORT_ORDER1" => "DESC",
+            "SORT_ORDER2" => "ASC",
+            "STRICT_SECTION_CHECK" => "N",
+            "FILTER_NAME" => "arrNewProducts"
+        )
+    );?>
     <div class="podium text-center"># PODIUMMARKET</div>
     <div class="girl text-center">
         <div class="girl__slide"><a href="#"><img src="<?=StockMan\Config::STOCKMAN_TEMPLATE_PATH?>/i/g1.png" alt=""></a></div>
--- www/local/include/app/Config.php
+++ www/local/include/app/Config.php
@@ -13,9 +13,17 @@ class Config
 
     //ID Каталога
     const CATALOG_ID = 2;
+    const CATALOG_TYPE = "catalog";
 
     /*----------Меню--------*/
     const INDEX_CATALOG_SECTION_ID_1 = 0;
     const INDEX_CATALOG_SECTION_ID_2 = 1;
     /*----------------------*/
+
+    //Свойства каталога
+    const NEW_PRODUCT = "NEWPRODUCT";
+    const BREND = "BRAND_REF";
+
+    //ID High-load блока с брендами
+    const BRAND_REF_BLOCK_ID = 2;
 }
