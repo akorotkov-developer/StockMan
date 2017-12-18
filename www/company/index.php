7bbebb04b414f1c77339569d8e76ec9bcd995e3d
k.sumerin <katanakidnapper@gmail.com>
2017-12-18 15:43:34 +0300

    Информационные страницы

---
+++ www/company/index.php
@@ -0,0 +1,24 @@
+<?
+require($_SERVER["DOCUMENT_ROOT"]."/bitrix/header.php");
+$APPLICATION->SetTitle("О компании");
+?>
+<div class="grid-container">
+<p><strong>PODIUM market&reg;&nbsp;&mdash; магазин, где в одном месте собраны более пятисот российских и мировых брендов для настоящих ценителей качества и уникальных дизайнерских решений!</strong></p>
+<p>Мы существуем с 2012 года, но за это время мы открыли пять универмагов в Москве, один в Казани, один в Сургуте и один в Краснодаре, и это не только простая навигация по магазину, но и тематически оформленные корнеры, дружелюбные консультанты и профессиональные стилисты.</p>
+<p>Каждый магазин PODIUM market&reg;&nbsp;&mdash; это синоним словосочетания «модно и недорого», а также несколько тысяч квадратных метров доступной роскоши: качественных и красивых вещей за разумные деньги. Женская и мужская одежда, обувь и аксессуары горячо полюбившихся московской публике брендов Sandro, Maje, Claudie Pierlot, Whistles, Reiss, Ted Baker, Bimba & Lola, Brigitte Bardot, American Vintage, Free People, Scotch & Soda, ELEVEN PARIS, MOODS OF NORWAY, Ash, Kate Spade, United Nude, Sixty Seven, Satchels, Rebecca Minkoff, Michael by Michael Kors.</p>
+<p>В сети магазинов представлено детское направление - PODIUM market kids&reg;, в котором можно найти   детскую одежду, обувь и аксессуары для маленьких модников и модниц Scotch&Soda, ELEVEN PARIS, Petit Bateau, Mini Rodini, Noa Noa, Japan Rags, Finger In the Nose, Morley, How To Kiss a Frog, EMU, Sorel, Converse. Кроме того, в детском корнере  PODIUM market&reg; представлен широкий выбор товаров для новорожденных, развивающих игр, наборов для творчества, конструкторов и книг  Aden+Anais, Moulin Roty, Clever, Nanoblock.</p>
+<p>Корнеры PODIUM Sport&reg; в Podium market&reg;&nbsp;&mdash; это стиль, комфорт и функциональность — три основных условия, по которым отбирается одежда для наших магазинов. В бренд-портфолио PODIUM Sport&reg; входят Adidas by Stella McCartney, James Perse, Asics, Mizuno, Zoot, Odlo, Craft, 2XU, Skins, Arena, Speedo, Tyr, Zoggs, EA7 от Armani, Deha, Danza, Meeting, Striders Edge. PODIUM Sport&reg;&nbsp;&mdash; это проект для людей, которые не мыслят своей жизни без активных тренировок и выбирают только лучшую, качественную и стильную спортивную одежду.</p>
+<p>А с 2013 года мы запустили интернет-магазин, чтобы покупки у нас стали еще более приятными и удобными.</p>
+<h4>Реквизиты ОАО «СТОКМАНН»</h4>
+<p>Юридический адрес:<br>
+129085, г. Москва, проезд Ольминского, д.3а стр.3<br>
+Фактический адрес:<br>
+123290, г. Москва, 1-ый Магистральный тупик, д. 11<br>
+ИНН 7717145242<br></p>
+<p>Телефон интернет-магазина:<br>
+8 (495) 223-01-13,<br>
+8 (800)-555-49-05<br>
+Email: clients@podium-market.com<br>
+</p>
+</div>
+<?require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");?>
