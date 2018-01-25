ba5893b2bedca08de27f33f8dd70bc50262d0a7f
greatt169 <greatt169.92@mail.ru>
2018-01-25 09:52:58 +0300

    Фильтр каталога

--- www/local/templates/.default/components/bitrix/catalog/catalogpodiummarket/section.php
+++ www/local/templates/.default/components/bitrix/catalog/catalogpodiummarket/section.php
@@ -69,11 +69,10 @@ if ($isFilter)
 		$arCurSection = array();
 }
 ?>
-<div class="row">
+
 <?
 if ($isVerticalFilter)
 	include($_SERVER["DOCUMENT_ROOT"]."/".$this->GetFolder()."/section_vertical.php");
 else
 	include($_SERVER["DOCUMENT_ROOT"]."/".$this->GetFolder()."/section_horizontal.php");
 ?>
-</div>
