d4c1e35f96843fb2b78d421083ccb73d19ab270c
a.korotkov <a.korotkov@ceteralabs.com>
2018-03-14 17:49:20 +0300

    Редактирование подписки

---
+++ www/personal/subscribe/subscr_edit.php
@@ -0,0 +1,18 @@
+<?
+require($_SERVER["DOCUMENT_ROOT"]."/bitrix/header.php");
+$APPLICATION->SetTitle("Интернет-магазин \"Одежда\"");
+?>
+
+<?$APPLICATION->IncludeComponent(
+    "bitrix:subscribe.edit",
+    "",
+    Array(
+        "SHOW_HIDDEN" => "N",
+        "ALLOW_ANONYMOUS" => "Y",
+        "SHOW_AUTH_LINKS" => "Y",
+        "CACHE_TIME" => "36000000",
+        "SET_TITLE" => "Y"
+    )
+);?>
+
+<?require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");?>
