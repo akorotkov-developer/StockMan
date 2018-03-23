3a34b6181217d0b009aeca0b84804ebee37ab98c
v.mokin <v.mokin@ceteralabs.com>
2018-03-23 14:27:35 +0300

    urlrewrite.php

--- www/urlrewrite.php
+++ www/urlrewrite.php
@@ -1,41 +1,51 @@
-<?
-$arUrlRewrite = array(
-	array(
-		"CONDITION" => "#^/bitrix/services/ymarket/#",
-		"RULE" => "",
-		"ID" => "",
-		"PATH" => "/bitrix/services/ymarket/index.php",
-	),
-	array(
-		"CONDITION" => "#^/personal/#",
-		"RULE" => "",
-		"ID" => "bitrix:sale.personal.section",
-		"PATH" => "/personal/index.php",
-	),
-	array(
-		"CONDITION" => "#^/#",
-		"RULE" => "",
-		"ID" => "bitrix:catalog",
-		"PATH" => "/index.php",
-	),
-	array(
-		"CONDITION" => "#^/store/#",
-		"RULE" => "",
-		"ID" => "bitrix:catalog.store",
-		"PATH" => "/store/index.php",
-	),
-	array(
-		"CONDITION" => "#^/faq/#",
-		"RULE" => "",
-		"ID" => "bitrix:news",
-		"PATH" => "/faq/index.php",
-	),
-	array(
-		"CONDITION" => "#^/blog/#",
-		"RULE" => "",
-		"ID" => "bitrix:news",
-		"PATH" => "/blog/index.php",
-	),
+<?php
+$arUrlRewrite=array (
+  1 => 
+  array (
+    'CONDITION' => '#^/personal/#',
+    'RULE' => '',
+    'ID' => 'bitrix:sale.personal.section',
+    'PATH' => '/personal/index.php',
+    'SORT' => 10,
+  ),
+  2 => 
+  array (
+    'CONDITION' => '#^/#',
+    'RULE' => '',
+    'ID' => 'bitrix:catalog',
+    'PATH' => '/index.php',
+    'SORT' => 20,
+  ),
+  0 => 
+  array (
+    'CONDITION' => '#^/bitrix/services/ymarket/#',
+    'RULE' => '',
+    'ID' => '',
+    'PATH' => '/bitrix/services/ymarket/index.php',
+    'SORT' => 100,
+  ),
+  3 => 
+  array (
+    'CONDITION' => '#^/store/#',
+    'RULE' => '',
+    'ID' => 'bitrix:catalog.store',
+    'PATH' => '/store/index.php',
+    'SORT' => 100,
+  ),
+  5 => 
+  array (
+    'CONDITION' => '#^/blog/#',
+    'RULE' => '',
+    'ID' => 'bitrix:news',
+    'PATH' => '/blog/index.php',
+    'SORT' => 100,
+  ),
+  4 => 
+  array (
+    'CONDITION' => '#^/faq/#',
+    'RULE' => '',
+    'ID' => 'bitrix:news',
+    'PATH' => '/faq/index.php',
+    'SORT' => 100,
+  ),
 );
-
-?>
