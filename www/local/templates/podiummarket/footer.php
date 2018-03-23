c467daa5827f7aef048c68bf586bede9e03a072d
greatt169 <greatt169.92@mail.ru>
2018-03-23 14:07:31 +0300

    Регистрация во включаемой области

--- www/local/templates/podiummarket/footer.php
+++ www/local/templates/podiummarket/footer.php
@@ -21,23 +21,11 @@ if (strpos($APPLICATION->GetCurPage(), "personal/order/make") > 0) {
 
 
     <div class="reveal" id="reg-popup" data-reveal data-deep-link="true">
-        <?$APPLICATION->IncludeComponent("bitrix:main.register","",Array(
-                "USER_PROPERTY_NAME" => "",
-                "SEF_MODE" => "Y",
-                "SHOW_FIELDS" => Array("NAME", "LAST_NAME", "SECOND_NAME", "PERSONAL_GENDER", "PERSONAL_BIRTHDAY", "PERSONAL_PHONE", "UF_SUBSCRIBE"),
-                "REQUIRED_FIELDS" => Array("NAME","PERSONAL_PHONE"),
-                "AUTH" => "Y",
-                "USE_BACKURL" => "Y",
-                "SUCCESS_PAGE" => "",
-                "SET_TITLE" => "N",
-                "USER_PROPERTY" => Array(),
-                "SEF_FOLDER" => "/",
-                "VARIABLE_ALIASES" => Array(),
-                "AJAX_OPTION_JUMP" => "N",
-                "AJAX_OPTION_STYLE" => "Y",
-                "AJAX_OPTION_HISTORY" => "N",
-                "AJAX_OPTION_ADDITIONAL" => "",
-                "AJAX_MODE" => "Y",
+        <?$APPLICATION->IncludeComponent("bitrix:main.include","",Array(
+                "AREA_FILE_SHOW" => "file",
+                "PATH" => StockMan\Config::STOCKMAN_TEMPLATE_PATH."/include_areas/register.php",
+                "AREA_FILE_RECURSIVE" => "Y",
+                "EDIT_TEMPLATE" => ""
             )
         );?>
     </div>
