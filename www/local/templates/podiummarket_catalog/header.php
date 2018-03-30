7952d7c484d0da5d8f07827e4adeec04729c3711
v.mokin <v.mokin@ceteralabs.com>
2018-03-30 16:02:47 +0300

    https://pm.cetera.ru/browse/STMD-165

--- www/local/templates/podiummarket_catalog/header.php
+++ www/local/templates/podiummarket_catalog/header.php
@@ -8,18 +8,30 @@ $isSearch = false;
 if (isset($_REQUEST['q'])) {
     $isSearch = true;
 }
+$arPage = array();
 $isMain = false;
 if ($APPLICATION->GetCurPage() == "/") {
     $isMain = true;
-}
-?>
-<?if (($isSearch)and($isMain)) {
-   ?><div class="content content_medium"><?
 } else {
-    ?><div class="content <?$APPLICATION->ShowViewContent('catalog_element');?> padding-top-5 <?if ($isMain) {?>padding-bottom-0<?}?>"><?
-}?>
+    $arPageStr = explode('/',$APPLICATION->GetCurPage());
+    $arPage = array();
+    foreach ($arPageStr as $p) {
+        if (isset($p{1})) {
+            $arPage[] = $p;
+        }
+    }
+}
+
+?>
 <?
-if ((!$isMain)or($isSearch)) {
+    if (($isSearch)and($isMain)and(count($arPage) != 1)) {
+       ?><div class="content content_medium"><?
+    } else {
+        ?><div<?if (count($arPage) != 1){?> class="content <?$APPLICATION->ShowViewContent('catalog_element');?> padding-top-5 <?if ($isMain) {?>padding-bottom-0<?}?>"<?}?>><?
+    }
+?>
+<?
+if (((!$isMain)or($isSearch))and(count($arPage)>1)) {
     if ($isSearch) {?><div class="grid-container"><?}
     $APPLICATION->IncludeComponent("bitrix:breadcrumb", "",
         Array(
