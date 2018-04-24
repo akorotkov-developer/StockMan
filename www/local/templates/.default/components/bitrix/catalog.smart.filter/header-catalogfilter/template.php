3137b8b8b9fff4981f0d21e28aac87a26ed6a900
v.mokin <v.mokin@ceteralabs.com>
2018-04-24 11:14:41 +0300

    https://pm.cetera.ru/browse/STMD-228

--- www/local/templates/.default/components/bitrix/catalog.smart.filter/header-catalogfilter/template.php
+++ www/local/templates/.default/components/bitrix/catalog.smart.filter/header-catalogfilter/template.php
@@ -12,7 +12,7 @@
 /** @var CBitrixComponent $component */
 $this->setFrameMode(true);
 
-$arSearchable = ['TSVET', 'BRAND'];
+$arSearchable = ['TSVET', 'MARKA'];
 
 $arExcluded = ['DISCOUNT', StockMan\Catalog\Config::PROP_DISCOUNT];
 
